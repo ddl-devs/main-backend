46cb77f43660313000facc2cd3b9d1e7333f1f90
Lucas <lucasoliveirars22@gmail.com>
2024-12-03 20:34:04 -0300

    domain user

---
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/domain/User.java
@@ -0,0 +1,44 @@
+package br.com.ifrn.ddldevs.pets_backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String keycloakId;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = true)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = true)
+    private Date dateOfBirth;
+
+    @Column(nullable = true)
+    private String photoUrl;
+}
+
