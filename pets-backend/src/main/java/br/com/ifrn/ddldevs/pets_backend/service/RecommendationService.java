9238544928d43e05e03dda8449cb37588f1e0e88
Lucas <lucasoliveirars22@gmail.com>
2025-02-16 16:33:58 -0300

    :construction: protecting recommendation routes with PreAuthorize

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/RecommendationService.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/RecommendationService.java
@@ -4,16 +4,15 @@ import br.com.ifrn.ddldevs.pets_backend.domain.Pet;
 import br.com.ifrn.ddldevs.pets_backend.domain.Recommendation;
 import br.com.ifrn.ddldevs.pets_backend.dto.Recomendation.RecommendationRequestDTO;
 import br.com.ifrn.ddldevs.pets_backend.dto.Recomendation.RecommendationResponseDTO;
-
+import br.com.ifrn.ddldevs.pets_backend.exception.AccessDeniedException;
 import br.com.ifrn.ddldevs.pets_backend.exception.ResourceNotFoundException;
 import br.com.ifrn.ddldevs.pets_backend.mapper.RecommendationMapper;
 import br.com.ifrn.ddldevs.pets_backend.repository.PetRepository;
 import br.com.ifrn.ddldevs.pets_backend.repository.RecommendationRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 public class RecommendationService {
@@ -28,7 +27,10 @@ public class RecommendationService {
     private PetRepository petRepository;
 
     @Transactional
-    public RecommendationResponseDTO createRecommendation(RecommendationRequestDTO recommendationRequestDTO) {
+    public RecommendationResponseDTO createRecommendation(
+        RecommendationRequestDTO recommendationRequestDTO,
+        String loggedUserKeycloakId
+    ) {
         Long idPet = recommendationRequestDTO.getPetId();
         if (idPet == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
@@ -39,7 +41,9 @@ public class RecommendationService {
 
         Recommendation recommendation = recommendationMapper.toEntity(recommendationRequestDTO);
         Pet pet = petRepository.findById(recommendationRequestDTO.getPetId())
-                .orElseThrow(() -> new RuntimeException("User not found"));
+            .orElseThrow(() -> new RuntimeException("User not found"));
+
+        throwErrorIfPetBelongsToAnotherUser(pet, loggedUserKeycloakId);
 
         recommendation.setPet(pet);
         pet.getRecommendations().add(recommendation);
@@ -55,7 +59,7 @@ public class RecommendationService {
         return recommendationMapper.toDTOList(recommendations);
     }
 
-    public RecommendationResponseDTO getRecommendation(Long id) {
+    public RecommendationResponseDTO getRecommendation(Long id, String loggedUserKeycloakId) {
         if (id == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
@@ -64,11 +68,14 @@ public class RecommendationService {
         }
 
         Recommendation recommendation = recommendationRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Recommendation not found"));
+            .orElseThrow(() -> new RuntimeException("Recommendation not found"));
+
+        throwErrorIfPetBelongsToAnotherUser(recommendation.getPet(), loggedUserKeycloakId);
+
         return recommendationMapper.toRecommendationResponseDTO(recommendation);
     }
 
-    public void deleteRecommendation(Long id) {
+    public void deleteRecommendation(Long id, String loggedUserKeycloakId) {
         if (id == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
@@ -76,14 +83,17 @@ public class RecommendationService {
             throw new IllegalArgumentException("ID não pode ser negativo");
         }
 
-        if (!recommendationRepository.existsById(id)) {
+        Recommendation recommendation = recommendationRepository.findById(id).orElseThrow(() -> {
             throw new ResourceNotFoundException("Recomendação não encontrada!");
-        }
+        });
+
+        throwErrorIfPetBelongsToAnotherUser(recommendation.getPet(), loggedUserKeycloakId);
+
         recommendationRepository.deleteById(id);
     }
 
     @Transactional
-    public List<RecommendationResponseDTO> getAllByPetId(Long id) {
+    public List<RecommendationResponseDTO> getAllByPetId(Long id, String loggedUserKeycloakId) {
         if (id == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
@@ -91,7 +101,21 @@ public class RecommendationService {
             throw new IllegalArgumentException("ID não pode ser negativo");
         }
 
+        Pet pet = petRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Pet não encontrado!");
+        });
+
+        throwErrorIfPetBelongsToAnotherUser(pet, loggedUserKeycloakId);
+
         List<Recommendation> recommendations = recommendationRepository.findAllByPetId(id);
+
         return recommendationMapper.toDTOList(recommendations);
+    }
+
+    private void throwErrorIfPetBelongsToAnotherUser(Pet pet, String loggedUserKeycloakId) {
+        if (!pet.getUser().getKeycloakId().equals(loggedUserKeycloakId)) {
+            throw new AccessDeniedException(
+                "Você não pode acessar dados de pets de outros usuários!");
+        }
     }
 }
