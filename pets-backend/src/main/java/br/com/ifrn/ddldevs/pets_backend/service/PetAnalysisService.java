15315e32d5a3b26cc2e1e741405d2a74efd7048a
Lucas <lucasoliveirars22@gmail.com>
2025-02-16 16:10:41 -0300

    :construction: protecting petAnalysis routes with PreAuthorize

--- pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/PetAnalysisService.java
+++ pets-backend/src/main/java/br/com/ifrn/ddldevs/pets_backend/service/PetAnalysisService.java
@@ -5,17 +5,17 @@ import br.com.ifrn.ddldevs.pets_backend.domain.Pet;
 import br.com.ifrn.ddldevs.pets_backend.domain.PetAnalysis;
 import br.com.ifrn.ddldevs.pets_backend.dto.PetAnalysis.PetAnalysisRequestDTO;
 import br.com.ifrn.ddldevs.pets_backend.dto.PetAnalysis.PetAnalysisResponseDTO;
+import br.com.ifrn.ddldevs.pets_backend.exception.AccessDeniedException;
 import br.com.ifrn.ddldevs.pets_backend.exception.ResourceNotFoundException;
 import br.com.ifrn.ddldevs.pets_backend.mapper.PetAnalysisMapper;
 import br.com.ifrn.ddldevs.pets_backend.repository.PetAnalysisRepository;
 import br.com.ifrn.ddldevs.pets_backend.repository.PetRepository;
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,7 +31,10 @@ public class PetAnalysisService {
     private PetRepository petRepository;
 
     @Transactional
-    public PetAnalysisResponseDTO createPetAnalysis(PetAnalysisRequestDTO petAnalysisRequestDTO) {
+    public PetAnalysisResponseDTO createPetAnalysis(
+        PetAnalysisRequestDTO petAnalysisRequestDTO,
+        String loggedUserKeycloakId
+    ) {
         Long idPet = petAnalysisRequestDTO.getPetId();
         if (idPet == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
@@ -42,7 +45,9 @@ public class PetAnalysisService {
 
         PetAnalysis petAnalysis = petAnalysisMapper.toEntity(petAnalysisRequestDTO);
         Pet pet = petRepository.findById(petAnalysisRequestDTO.getPetId())
-                .orElseThrow(() -> new RuntimeException("Pet not found"));
+            .orElseThrow(() -> new RuntimeException("Pet not found"));
+
+        throwErrorIfPetBelongsToAnotherUser(pet, loggedUserKeycloakId);
 
         petAnalysis.setPet(pet);
         pet.getPetAnalysis().add(petAnalysis);
@@ -58,20 +63,24 @@ public class PetAnalysisService {
         return petAnalysisMapper.toResponseList(petAnalyses);
     }
 
-    public void deletePetAnalysis(Long id) {
+    public void deletePetAnalysis(Long id, String loggedUserKeycloakId) {
         if (id == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
         if (id < 0) {
             throw new IllegalArgumentException("ID não pode ser negativo");
         }
-        if (!petAnalysisRepository.existsById(id)) {
-            throw new ResourceNotFoundException("Análise não encontrada!");
-        }
+
+        PetAnalysis petAnalysis = petAnalysisRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Análise não encontrada");
+        });
+
+        throwErrorIfPetBelongsToAnotherUser(petAnalysis.getPet(), loggedUserKeycloakId);
+
         petAnalysisRepository.deleteById(id);
     }
 
-    public List<PetAnalysisResponseDTO> getAllByPetId(Long id) {
+    public List<PetAnalysisResponseDTO> getAllByPetId(Long id, String loggedUserKeycloakId) {
         if (id == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
@@ -80,10 +89,16 @@ public class PetAnalysisService {
         }
 
         List<PetAnalysis> petAnalyses = petAnalysisRepository.findAllByPetId(id);
+
+        Pet pet = petRepository.findById(id)
+            .orElseThrow(() -> new ResourceNotFoundException("Pet não encontrado"));
+
+        throwErrorIfPetBelongsToAnotherUser(pet, loggedUserKeycloakId);
+
         return petAnalysisMapper.toResponseList(petAnalyses);
     }
-    
-    public PetAnalysisResponseDTO getPetAnalysis(Long analysisId) {
+
+    public PetAnalysisResponseDTO getPetAnalysis(Long analysisId, String loggedUserKeycloakId) {
         if (analysisId == null) {
             throw new IllegalArgumentException("ID não pode ser nulo");
         }
@@ -91,7 +106,17 @@ public class PetAnalysisService {
             throw new IllegalArgumentException("ID não pode ser negativo");
         }
         PetAnalysis petAnalysis = petAnalysisRepository.findById(analysisId)
-                .orElseThrow(() -> new EntityNotFoundException("Pet Analysis not found"));
+            .orElseThrow(() -> new EntityNotFoundException("Pet Analysis not found"));
+
+        throwErrorIfPetBelongsToAnotherUser(petAnalysis.getPet(), loggedUserKeycloakId);
+
         return petAnalysisMapper.toResponse(petAnalysis);
+    }
+
+    private void throwErrorIfPetBelongsToAnotherUser(Pet pet, String loggedUserKeycloakId) {
+        if (!pet.getUser().getKeycloakId().equals(loggedUserKeycloakId)) {
+            throw new AccessDeniedException(
+                "Você não pode acessar dados de pets de outros usuários!");
+        }
     }
 }
